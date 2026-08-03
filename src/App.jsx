b95a2575ceae89f1003d4eb27f8a852c1dc137f0
Nattkorn Saisuoy <91656159+banksaisuoy@users.noreply.github.com>
2026-08-03 19:01:57 +0700

    update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { AppRouter } from './router';
+import { AuthProvider } from './auth/AuthProvider';
 import './index.css';
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  );
 };
 
 export default App;

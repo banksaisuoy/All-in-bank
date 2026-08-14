166ba773cd5e88fbdb412bd92dbe0aed61621096
Nattkorn Saisuoy <91656159+banksaisuoy@users.noreply.github.com>
2026-08-15 03:50:42 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.js'],
-    globals: true,
-    coverage: {
-      provider: 'v8',
       exclude: ['src/**/*.test.{js,jsx}', 'src/main.jsx', 'src/vite-env.d.ts'],
     },
   },
+});

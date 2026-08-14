94b3cf21d5c8e095e65e81b14fb73c08bc4fe070
Nattkorn Saisuoy <91656159+banksaisuoy@users.noreply.github.com>
2026-08-15 01:32:47 +0700

    update src/services/transactionService.js

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,4 +1,9 @@
-      ]);
+import { mockTransactions } from '../lib/mockData';
+
+export const getRecentTransactions = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mockTransactions.slice(0, 5));
     }, 500); // simulate network delay
   });
 };

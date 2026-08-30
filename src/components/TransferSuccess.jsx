8e5ff16c131c0e10a7c5bd979b917c09cfece549
Nattkorn Saisuoy <91656159+banksaisuoy@users.noreply.github.com>
2026-08-30 20:12:00 +0700

    update src/components/TransferSuccess.jsx

--- src/components/TransferSuccess.jsx
+++ src/components/TransferSuccess.jsx
@@ -1,8 +1,7 @@
-            {transferDetails.description && (
               <div className="sm:col-span-2">
                 <dt className="text-sm font-medium text-gray-500">Description</dt>
                 <dd className="mt-1 text-sm text-gray-900 break-words">
-                  {transferDetails.description.replace(/[<>]/g, '')}
+                  {transferDetails.description}
                 </dd>
               </div>
             )}
